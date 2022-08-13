0e5a9b8f6e12b5fd53c143588ef3f74321aa6343
chukimmuoi <chukimmuoi@gmail.com>
2022-08-13 14:59:10 +0700

    Add Button

--- app/src/main/java/com/example/unitconverterapp/TopScreen.kt
+++ app/src/main/java/com/example/unitconverterapp/TopScreen.kt
@@ -1,5 +1,6 @@
 package com.example.unitconverterapp
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -28,6 +29,8 @@ fun TopScreen(list: List<Conversion>) {
     }
 
     selectedConversion.value?.let {
-        InputBlock(conversion = it, inputText = inputText)
+        InputBlock(conversion = it, inputText = inputText) { input ->
+            Log.i("MY_TAG", "User typed $input")
+        }
     }
 }
